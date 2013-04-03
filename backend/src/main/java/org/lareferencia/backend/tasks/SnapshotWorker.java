d5eb6355ba680e4a7620a81ad1e04bcebe21c21a
lmatas <lmatas@gmail.com>
2013-04-03 17:29:54 -0300

    Cambios en SnapshotWorker para adaptarlo al nuevo esquema de resultados de harvesting

--- backend/src/main/java/org/lareferencia/backend/tasks/SnapshotWorker.java
+++ backend/src/main/java/org/lareferencia/backend/tasks/SnapshotWorker.java
@@ -1,11 +1,11 @@
 package org.lareferencia.backend.tasks;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+import java.util.Map.Entry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.xml.transform.TransformerException;
 
 import org.lareferencia.backend.domain.NationalNetwork;
 import org.lareferencia.backend.domain.NetworkSnapshot;
@@ -14,17 +14,18 @@ import org.lareferencia.backend.domain.OAIRecord;
 import org.lareferencia.backend.domain.OAISet;
 import org.lareferencia.backend.domain.SnapshotStatus;
 import org.lareferencia.backend.harvester.HarvestingEvent;
-import org.lareferencia.backend.harvester.HarvestingEventStatus;
 import org.lareferencia.backend.harvester.IHarvester;
 import org.lareferencia.backend.harvester.IHarvestingEventListener;
 import org.lareferencia.backend.repositories.NationalNetworkRepository;
 import org.lareferencia.backend.repositories.NetworkSnapshotRepository;
 import org.lareferencia.backend.repositories.OAIRecordDAO;
-import org.lareferencia.backend.repositories.OAIRecordRepository;
+import org.lareferencia.backend.util.DCMetadataDOMHelper;
+import org.lareferencia.backend.util.IMetadataDOMHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Node;
 
 @Component
 @Scope(value="prototype")
@@ -42,6 +43,8 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 	@Autowired
 	private OAIRecordDAO recordDAO;
 	
+	private IMetadataDOMHelper domHelper;
+	
 	private IHarvester harvester;
 	
 	private Long _network_id;
@@ -55,6 +58,17 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 	}
 
 	public SnapshotWorker() {
+		
+		/** 
+		 * TODO: // El domHelper es el encargado de interpretar el dom de acuerdo a cada
+		 * schema de metadatatos, puede implementarse un factory en spring para crearlos
+		 * e injectarlos. Solo se haría necesario para soportar otros schemas de metadatos.
+		 * Incluso puede definirse en runtime para cada red, permitiendo distintas redes
+		 * con distintos metadatos.
+		 * Al ser un requerimiento de baja prioridad queda para el final o futuras implementaciones.
+		 * 
+		 * **/
+		domHelper = new DCMetadataDOMHelper();
 	};
 	
 	
@@ -87,7 +101,7 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 		
 		snapshot = new NetworkSnapshot();
 		network.getSnapshots().add(snapshot);
-
+	
 		snapshotRepository.save(snapshot);
 		networkRepository.save(network);
 		
@@ -104,7 +118,6 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 	}
 	
 	
-	
 	@Override
 	@Transactional
 	public void harvestingEventOccurred(HarvestingEvent event) {
@@ -115,17 +128,19 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 			case OK:			
 				// Agrega los records al snapshot actual
 				
-				
-				/*
-				for (OAIRecord record:event.getRecords() ) {
-		    		record.setSnapshot(snapshot);		
-		    		recordDAO.store(record);
+				for (Entry<String, Node> entry:event.getRecords().entrySet() ) {
+		  
+					try {
+						OAIRecord record = new OAIRecord(entry.getKey(), domHelper.Node2XMLString(entry.getValue()));
+						record.setSnapshot(snapshot);		
+			    		recordDAO.store(record);
+						
+					} catch (TransformerException e) {
+						e.printStackTrace();
+					}
 				}
-					recordDAO.flushAndClear();
-				 */
 				
-
-				recordDAO.store( event.getRecords(), snapshot );
+				//recordDAO.store( event.getRecords(), snapshot );
 				
 				snapshot.setSize( snapshot.getSize() + event.getRecords().size() );	
 				snapshotRepository.save(snapshot);
@@ -151,10 +166,7 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 				 * TODO: Definir que se hace en caso de eventos sin status conocido
 				 */	
 			break;
-		}
-		
-		
-		
+		}	
 	}
 	
 	private void launchHarvesting() {
@@ -176,11 +188,8 @@ public class SnapshotWorker implements ISnapshotWorker, IHarvestingEventListener
 			// si no hay set declarado cosecha todo
 			else {
 				harvester.harvest(origin.getUri(), null, null, null, origin.getMetadataPrefix());
-			}
-				
-	
-			
-	}	
+			}	
+		}	
 	}
 	/******************** Fin Harvesting *********************/
 	
