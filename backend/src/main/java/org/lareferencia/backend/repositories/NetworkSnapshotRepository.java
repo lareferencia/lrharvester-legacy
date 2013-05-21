4aee2a92b29c4a49a729d5e68b687b536d7ca7b6
lmatas <lmatas@gmail.com>
2013-05-21 13:54:12 -0300

    BugFix: LGKNSnapshot

--- backend/src/main/java/org/lareferencia/backend/repositories/NetworkSnapshotRepository.java
+++ backend/src/main/java/org/lareferencia/backend/repositories/NetworkSnapshotRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NetworkSnapshotRepository extends JpaRepository<NetworkSnapshot, Long> { 
 	
-	  @Query("select ns from NetworkSnapshot ns where ns.network.id = ?1 and ns.status = 4 and ns.endTime >= (select max(s.endTime) from NetworkSnapshot s where s.network.id = ?1 and ns.status = 4)")
+	  @Query("select ns from NetworkSnapshot ns where ns.network.id = ?1 and ns.status = 4 and ns.endTime >= (select max(s.endTime) from NetworkSnapshot s where s.network.id = ?1 and s.status = 4)")
 	  NetworkSnapshot findLastGoodKnowByNetworkID(Long networkID);
 	  
 	  List<NetworkSnapshot> findByNetworkAndStatusOrderByEndTimeAsc(NationalNetwork network, SnapshotStatus status);
