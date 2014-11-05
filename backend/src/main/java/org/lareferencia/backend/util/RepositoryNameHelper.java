62a56b3f0ff94c0f7e1ff56ab53f27b391a69ace
lmatas <lmatas@gmail.com>
2014-11-05 19:11:41 -0300

    modificación de la estrategia de detección de reponame y implementación de append de reponame/instname

---
+++ backend/src/main/java/org/lareferencia/backend/util/RepositoryNameHelper.java
@@ -0,0 +1,71 @@
+package org.lareferencia.backend.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.lareferencia.backend.harvester.OAIRecordMetadata;
+import org.w3c.dom.Node;
+
+public class RepositoryNameHelper {
+	
+	private  Pattern pattern = null;
+	
+	public RepositoryNameHelper() {
+		try {
+			pattern = Pattern.compile(DOMAIN_NAME_PATTERN_STR);
+		}
+		catch (PatternSyntaxException e) {
+			System.err.println("RepositoryNameHelper::Error en el patron: " + DOMAIN_NAME_PATTERN_STR);
+			
+		}
+		
+	}
+	
+	public void setDetectREPattern(String patternString) {
+		
+		try {
+			pattern = Pattern.compile(patternString);
+		} catch (PatternSyntaxException e) {
+			System.err.println("RepositoryNameHelper::Error en el patron: " + patternString);
+			
+		}
+	}
+
+	public static String UNKNOWN = "Unknown"; 
+
+	public static final String DOMAIN_NAME_PATTERN_STR = "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z-]{2,})";
+	private static final String NAME_PATTERN_STR = "[A-Za-z0-9-]{4,}";
+
+	public String detectRepositoryDomain(String identifier) {
+
+		String result = UNKNOWN;
+
+		Matcher matcher = pattern.matcher(identifier);
+
+		if (matcher.find())
+			result = matcher.group();
+		
+		return result;
+	}
+	
+	public String extractNameFromMetadata(OAIRecordMetadata metadata, String fieldname, String prefix) {
+		
+		String name = UNKNOWN;
+		
+		for ( Node node: metadata.getFieldNodes(fieldname) ) {
+			
+			String occr = node.getFirstChild().getNodeValue();
+
+			if ( occr.startsWith(prefix) )
+				name = occr.substring(prefix.length()).trim();	
+		}
+		
+		return name;
+	}
+	
+	public void appendNameToMetadata(OAIRecordMetadata metadata, String fieldname, String prefix, String value) {
+    	metadata.addFieldOcurrence(fieldname, prefix + value);
+	}
+	
+}
