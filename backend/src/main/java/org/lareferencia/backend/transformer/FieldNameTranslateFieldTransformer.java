c8a4560e4eb1b4020edd80f407e873213ae5fa1f
lmatas <lmatas@gmail.com>
2015-03-03 12:50:32 -0300

    Transformador de nombres de campos. Test para UNColombia

---
+++ backend/src/main/java/org/lareferencia/backend/transformer/FieldNameTranslateFieldTransformer.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2015
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Lautaro Matas (lmatas@gmail.com) - Desarrollo e implementación
+ * 
+ ******************************************************************************/
+
+package org.lareferencia.backend.transformer;
+
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.lareferencia.backend.harvester.OAIRecordMetadata;
+import org.w3c.dom.Node;
+
+public class FieldNameTranslateFieldTransformer extends FieldTransformer {
+
+	@Getter
+	Map<String, String> translationMap;
+
+	@Setter
+	@Getter
+	String sourceFieldName;
+	
+	@Setter
+	@Getter
+	String targetFieldName;
+	
+
+	public FieldNameTranslateFieldTransformer() {
+		this.applyIfValid = false;
+	}
+
+	@Override
+	public boolean transform(OAIRecordMetadata metadata) {
+
+		boolean wasTransformed = false;
+	
+		// ciclo de reemplazo
+		// recorre las ocurrencias del campo de nombre source creando instancias con nombre target
+		for (Node node : metadata.getFieldNodes(this.getSourceFieldName())) {
+			
+			// Agrega instancia target con el contenido a reemplazar
+			String occr = node.getFirstChild().getNodeValue();
+			metadata.addFieldOcurrence(this.getTargetFieldName(), occr);
+			
+			// Remueve la actual
+			Node parentNode = node.getParentNode();
+			parentNode.removeChild(node);
+			
+			// si entra al ciclo al menos una vez entonces transformó
+			wasTransformed = true;
+		}
+
+		return wasTransformed;
+	}
+
+	
+
+}
