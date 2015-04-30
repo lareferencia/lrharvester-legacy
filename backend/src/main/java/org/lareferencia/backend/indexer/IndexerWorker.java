8fc6b1924f8dc358f3f8df0cef168a36c9ba5222
Lautaro Matas <lmatas@gmail.com>
2015-04-30 11:12:02 -0300

    Primera alpha indexador xoai

--- backend/src/main/java/org/lareferencia/backend/indexer/IndexerWorker.java
+++ backend/src/main/java/org/lareferencia/backend/indexer/IndexerWorker.java
@@ -13,37 +13,14 @@
  ******************************************************************************/
 package org.lareferencia.backend.indexer;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import lombok.Getter;
 import lombok.Setter;
 
-import org.lareferencia.backend.domain.Network;
 import org.lareferencia.backend.domain.NetworkSnapshot;
-import org.lareferencia.backend.domain.OAIOrigin;
-import org.lareferencia.backend.domain.OAIRecord;
-import org.lareferencia.backend.domain.OAISet;
-import org.lareferencia.backend.domain.RecordStatus;
-import org.lareferencia.backend.domain.SnapshotStatus;
-import org.lareferencia.backend.harvester.HarvestingEvent;
-import org.lareferencia.backend.harvester.IHarvester;
-import org.lareferencia.backend.harvester.IHarvestingEventListener;
-import org.lareferencia.backend.harvester.OAIRecordMetadata;
-import org.lareferencia.backend.repositories.NetworkRepository;
 import org.lareferencia.backend.repositories.NetworkSnapshotRepository;
-import org.lareferencia.backend.repositories.OAIRecordRepository;
-import org.lareferencia.backend.transformer.ITransformer;
-import org.lareferencia.backend.validator.IValidator;
-import org.lareferencia.backend.validator.ValidationResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Scope(value="prototype")
@@ -57,6 +34,7 @@ public class IndexerWorker implements Runnable {
 	private NetworkSnapshotRepository networkSnapshotRepository;
 	
 	@Autowired
+	@Qualifier("indexer")
 	IIndexer indexer;
 	
 	public IndexerWorker() {
