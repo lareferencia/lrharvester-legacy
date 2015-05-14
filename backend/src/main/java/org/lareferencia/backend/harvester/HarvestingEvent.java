b97e019c94bf0ca3f1b9a2ef38e2c227e4f9627f
lmatas <lmatas@gmail.com>
2015-05-14 13:51:10 -0300

    FIX TEMPORAL: Bug caso resumption token > 255 generado por provider antiguo

--- backend/src/main/java/org/lareferencia/backend/harvester/HarvestingEvent.java
+++ backend/src/main/java/org/lareferencia/backend/harvester/HarvestingEvent.java
@@ -40,7 +40,9 @@ public class HarvestingEvent {
 			HarvestingEventStatus status, String resumptionToken) {
 		this.records = records;
 		this.status = status;
-		this.resumptionToken = resumptionToken;
+		this.resumptionToken = resumptionToken.substring(0, Math.min(resumptionToken.length()-1, 254));
+		// FIXME: Esto evita el problema con la restricción de resumption token mayor a 255
+		
 	}
 	
 	public HarvestingEvent(String msg,
